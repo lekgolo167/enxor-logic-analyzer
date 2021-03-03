153770b6bc1d8680a23a22fef1317c4444311227
lekgolo167 <167sonoma182@gmail.com>
2021-03-03 02:27:42 -0700

    programmable buffer logic

---
+++ hardware/enexor-logic-analyzer.srcs/sources_1/new/Data_Buffers_Programmable.v
@@ -0,0 +1,239 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/02/2021 11:07:47 AM
+// Design Name: 
+// Module Name: Data_Buffers_Programmable
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Data_Buffers_Programmable #(PACKET_WIDTH = 16, DEPTH = 16)(
+    input i_sys_clk,
+    input i_rstn,
+    input i_enable,
+    input i_stop,
+    input [$clog2(DEPTH)-1:0] i_pre_cap_depth,
+    input i_triggered_state,
+    input i_event,
+    input i_r_ack,
+    input i_start_read,
+    input [PACKET_WIDTH-1:0] i_data,
+    output reg o_post_read,
+    output reg o_buffer_full,
+    output reg o_finished_read,
+    output [PACKET_WIDTH-1:0] o_data,
+    output reg o_t_rdy
+);
+
+    localparam ADDR_WIDTH = $clog2(DEPTH);
+
+    localparam s_INIT = 3'b000;
+    localparam s_PRE_FILL = 3'b001;
+    localparam s_PRE_CAPTURE = 3'b010;
+    localparam s_POST_CAPTURE = 3'b100;
+    localparam s_WAIT = 3'b111;
+    localparam s_READ_PRE = 3'b001;
+    localparam s_READ_POST = 3'b010;
+    localparam s_WAIT_ACK = 3'b100;
+
+    reg r_wr_en;
+    reg [2:0] r_wr_state, r_rd_state, r_prev_state;
+    reg [ADDR_WIDTH-1:0] r_end_ptr, r_triggered_mark_ptr, r_wr_adr;
+    reg [ADDR_WIDTH-1:0] r_rd_adr;
+    
+    // writting buffer state machine
+    always @(posedge i_sys_clk) begin
+        if(!i_rstn) begin
+            r_wr_state <= s_INIT;
+        end
+        else 
+        begin
+            case(r_wr_state)
+                
+                s_INIT :
+                    begin
+                        // initialize write signals
+                        r_wr_en <= 0;
+                        r_end_ptr <= 0;
+                        r_wr_adr <= 0;
+                        o_buffer_full <= 0;
+                        if (i_enable) begin // wait here until the logic analyzer is enabled then enable write
+                            r_wr_en <= 1;
+                            r_wr_state <= s_PRE_FILL;
+                        end
+                    end
+                
+                s_PRE_FILL :
+                    begin
+                        r_wr_en <= 1;
+                        if (i_event) begin
+                            r_wr_adr <= r_wr_adr + 1;
+                            // mark last written to precapture address
+                            r_triggered_mark_ptr <= r_wr_adr; 
+                        end
+
+                        if (i_stop) begin
+                            r_wr_state <= s_INIT;
+                        end
+                        // trigger event happened, precapture buffer is smaller than programmed size
+                        else if (i_triggered_state) begin 
+                            r_wr_state <= s_POST_CAPTURE;
+                        end
+                        // precapture buffer has grown to max size
+                        else if (r_wr_adr == i_pre_cap_depth) begin 
+                            r_wr_state <= s_PRE_CAPTURE;
+                        end
+                    end
+
+                s_PRE_CAPTURE :
+                    begin
+                        if (i_event) begin
+                            // circular buffer, constantly overwritten until trigger event
+                            r_end_ptr <= r_end_ptr + 1;
+                            r_wr_adr <= r_wr_adr + 1;
+                            // mark last written precapture address
+                            r_triggered_mark_ptr <= r_wr_adr;
+                        end
+                        if (i_stop) begin
+                            r_wr_state <= s_INIT;
+                        end
+                        else if (i_triggered_state) begin
+                            r_wr_state <= s_POST_CAPTURE;
+                        end
+                    end
+
+                s_POST_CAPTURE:
+                    begin
+                        // fill buffer until write address wraps back around to first location written to
+                        if ((r_wr_adr == r_end_ptr) | i_stop) begin
+                            r_wr_state <= s_WAIT;
+                            o_buffer_full <= 1;
+                            r_wr_en <= 0;
+                        end
+                        else if (i_event) begin
+                            r_wr_adr <= r_wr_adr + 1;
+                        end
+                    end
+
+                s_WAIT:
+                    begin
+                        if (!i_enable) begin
+                            r_wr_state <= s_INIT;
+                        end
+                    end
+
+                default :
+                    r_wr_state <= s_INIT;
+            endcase    
+        end
+    end // End always
+    
+
+    // reading buffer state machine
+    always @(posedge i_sys_clk) begin
+        if(!i_rstn) begin
+            r_rd_state <= s_INIT;
+            r_prev_state <= s_WAIT_ACK;
+        end
+        else begin
+            case(r_rd_state)
+                s_INIT :
+                    begin
+                        if(i_start_read) begin
+                            o_t_rdy <= 1;
+                            r_rd_state <= s_READ_PRE;
+                        end
+                        // start reading from the oldest memory location
+                        r_rd_adr <= r_end_ptr;
+                        o_t_rdy <= 0;
+                        o_post_read <= 0;
+                        o_finished_read <= 0;
+                    end
+
+                s_READ_PRE:
+                    begin
+                        // signal that data is valid
+                        o_t_rdy <= 1;
+                        
+                        // once read address = last
+                        if(r_rd_adr == r_triggered_mark_ptr) begin
+                            o_t_rdy <= 0;
+                            r_rd_state <= s_WAIT_ACK;
+                            r_prev_state <= s_READ_POST;
+                        end
+                        // wait for ack then move to next memory location
+                        else if (i_r_ack) begin
+                            o_t_rdy <= 0;
+                            r_prev_state <= r_rd_state;
+                            r_rd_state <= s_WAIT_ACK;
+                            r_rd_adr <= r_rd_adr + 1;
+                        end
+                    end
+
+                s_READ_POST:
+                    begin
+                        o_t_rdy <= 1;
+                        
+                        // once read address has reach last written location, done reading
+                        if(r_rd_adr == r_wr_adr) begin
+                            o_t_rdy <= 0;
+                            r_rd_state <= s_WAIT_ACK;
+                            r_prev_state <= s_WAIT;
+                            o_finished_read <= 1;
+                        end
+                        // wait for ack then move to next memory location
+                        else if (i_r_ack) begin
+                            o_post_read <= 1;
+                            o_t_rdy <= 0;
+                            r_prev_state <= r_rd_state;
+                            r_rd_state <= s_WAIT_ACK;
+                            r_rd_adr <= r_rd_adr + 1;
+                        end
+                    end
+                    
+                s_WAIT_ACK:
+                    begin
+                        // wair for r_ack to deassert
+                        if (!i_r_ack) begin
+                            r_rd_state <= r_prev_state;
+                        end
+                    end
+                
+                s_WAIT:
+                    begin
+                        if (!i_enable) begin
+                            r_rd_state <= s_INIT;
+                        end
+                    end
+
+                default :
+                    r_rd_state <= s_INIT;
+
+            endcase
+            
+        end
+    end // End always
+    
+    sram #(.ADDR_WIDTH(ADDR_WIDTH), .DATA_WIDTH(PACKET_WIDTH), .DEPTH(DEPTH)) sram_0 (
+        .i_sys_clk(i_sys_clk),
+        .i_wr_en(r_wr_en & i_event),
+        .i_wr_adr(r_wr_adr),
+        .i_rd_adr(r_rd_adr),
+        .i_data(i_data),
+        .o_data(o_data)
+    );
+    
+endmodule
---
+++ hardware/enexor-logic-analyzer.srcs/sources_1/new/prog_databuffer_tb.v
@@ -0,0 +1,134 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/02/2021 07:53:32 PM
+// Design Name: 
+// Module Name: prog_databuffer_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module prog_databuffer_tb;
+
+    reg clk, rst, enable, start_read, r_ack, stop;
+    wire [7:0] w_time, w_channels;
+    reg [11:0] count;
+    
+    wire buffer_full, w_sample_clk_posedge, w_rollover, w_triggered_state, w_event, finished_read, post_read, t_rdy;
+    wire [15:0] data;
+    
+    Pulse_Sync #(.DATA_WIDTH(8))PS (
+        .i_sys_clk(clk),
+        .i_async(count[11:4]),
+        .o_sync(w_channels)
+    );
+    
+    Clock_Divider CD (
+        .i_sys_clk(clk),
+        .i_rstn(rst),
+        .i_scaler(5),
+        .o_sample_clk_posedge(w_sample_clk_posedge)
+    );
+    
+    Timestamp_Counter TSC (
+        .i_sys_clk(clk),
+        .i_rstn(rst),
+        .i_incr(w_sample_clk_posedge),
+        .i_event(w_event),
+        .o_rollover(w_rollover),
+        .o_time(w_time)    
+    );
+    
+    Trigger_Controller #(.DATA_WIDTH(8)) TC (
+        .i_sys_clk(clk),
+        .i_rstn(rst),
+        .i_data(w_channels),
+        .i_channel_select(4),
+        .i_trigger_type(1),
+        .i_enable(enable),
+        .i_sample_clk_posedge(w_sample_clk_posedge),
+        .o_triggered_state(w_triggered_state),
+        .o_event_pulse(w_event)
+        );
+        
+    Data_Buffers_Programmable #(.PACKET_WIDTH(16), .PRE_DEPTH(4), .POST_DEPTH(12)) DUT (
+        .i_sys_clk(clk),
+        .i_rstn(rst),
+        .i_enable(enable),
+        .i_stop(stop),
+        .i_triggered_state(w_triggered_state),
+        .i_event(w_event | w_rollover),
+        .i_r_ack(r_ack),
+        .i_start_read(start_read),
+        .i_data({w_time, w_channels}),
+        .i_pre_cap_depth(4),
+        .o_post_read(post_read),
+        .o_buffer_full(buffer_full),
+        .o_finished_read(finished_read),
+        .o_data(data),
+        .o_t_rdy(t_rdy)
+    );
+    
+    always
+        #2 clk = ~clk;
+        
+    initial begin
+        clk = 0;
+        rst = 0;
+        enable =0;
+        count = 0;
+        stop = 0;
+        r_ack = 0;
+        start_read = 0;
+        #50 rst = 1;
+        #15 enable = 1;
+    end
+    
+//    always @(posedge clk) begin
+//        if(w_triggered_state) begin
+//        #200
+//            stop <= 1;
+//        end
+//    end
+    
+    always @(posedge clk) begin
+        
+        
+        if (finished_read) begin
+            r_ack <= 0;
+            #250 start_read <= 0;
+            #25 enable <= 0;
+            #100 $finish;     
+        end
+        else if(buffer_full && !start_read) begin
+            #100 start_read <= 1;
+        end
+    end // End always
+
+    always @(posedge clk) begin
+        count <= count + 1;
+        if (start_read) begin
+            if(t_rdy) begin
+                r_ack <= 1;
+            end
+            else if (r_ack) begin
+            #20
+                r_ack <= 0;
+                #300; // Simulate send time through UART
+            end
+        end
+    end
+    
+endmodule
